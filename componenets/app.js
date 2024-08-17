0b3b0e88ee5c46dcb43aad6808a903e24d0e8e7b
aforosco <oregbemijohn77@gmail.com>
2024-08-17 19:15:27 +0100

    Refactoring to Api

--- componenets/app.js
+++ componenets/app.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Banner from "./banner";
 import HouseList from "./houseList";
+import House from "./house";
+
 const App = () => {
+    const [selectedHouse, setSelectedHouse] = useState();
     return(
         <>
         <Banner headerText = "Provinding Houses over the United Kingdom"/>
-        <HouseList/>
+        {selectedHouse ? <House house = {selectedHouse}/> : <HouseList/>}
         </>
     )
 };
--- componenets/houseList.js
+++ componenets/houseList.js
@@ -1,60 +1,52 @@
-import { useState } from "react";
+import loadingStatus from "../helpers/loadingStatus";
+import useHouses from "../hooks/useHouses";
 import HouseRow from "./houseRow";
-const houseArray = [
-    {
-      id: 1,
-      address: "12 Valley of Kings, Geneva",
-      country: "Switzerland",
-      price: 900000,
-    },
-    {
-      id: 2,
-      address: "89 Road of Forks, Bern",
-      country: "Switzerland",
-      price: 500000,
-    }
-  ];
+import LoadingIndicator from "./loadingIndicator";
 
-    const HouseList = () => {
-     const [houses, setHouses] = useState(houseArray);
-     const addHouse = () => {
-      setHouses([
-        ...houses,
-        {
-          id: 3,
-          address: "32 Valley Way, New York",
-          country: "USA",
-          price: 1000000,
-        },
-      ]);
-    };
+const HouseList = ({ selectHouse }) => {
+  const { houses, setHouses, loadingState } = useHouses();
 
-      return (
-      <>
-        <div className="row mb-2">
-          <h5 className="themeFontColor text-center">
-            Houses currently on the market
-          </h5>
-        </div>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>Address</th>
-              <th>Country</th>
-              <th>Asking Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {houses.map((h) => (
-            <HouseRow key={h.id} house={h} />
-          ))}
-          </tbody>
-        </table>
-        <button className="btn btn-primary" onClick={addHouse}>
-        Add
-      </button>
-      </>
-    );
+  if (loadingState !== loadingStatus.loaded)
+    return <LoadingIndicator loadingState={loadingState} />;
+
+  const addHouse = () => {
+    setHouses([
+      ...houses,
+      {
+        id: 3,
+        address: "32 Valley Way, New York",
+        country: "USA",
+        price: 1000000,
+      },
+    ]);
   };
 
-  export default HouseList;
+  return (
+    <>
+      <div className="row mb-2">
+        <h5 className="themeFontColor text-center">
+          Houses currently on the market
+        </h5>
+      </div>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Address</th>
+            <th>Country</th>
+            <th>Asking Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {houses.map((h) => (
+            <HouseRow key={h.id} house={h} selectHouse={selectHouse} />
+          ))}
+        </tbody>
+      </table>
+      <button className="btn btn-primary" onClick={addHouse}>
+        Add
+      </button>
+    </>
+  );
+};
+
+export default HouseList;
